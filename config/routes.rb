b74c4686fc480e270fe585af5566e396dc19e2c8
cowatea <earlgrey.cafe.tea@gmail.com>
2021-12-16 11:51:16 +0000

    make simple pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 Rails.application.routes.draw do
+  get 'home' => "static_pages#home"
+  get 'help' => "static_pages#help"
+  get 'about' => "static_pages#about"
+  get 'contact' => "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 以下のコードを追加してみましょう
