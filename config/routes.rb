6ecfce3492cc3db1565929e0e6e4002d5d96e5b6
cowatea <earlgrey.cafe.tea@gmail.com>
2021-12-16 18:26:39 +0000

    user controller and model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -9,9 +10,9 @@ Rails.application.routes.draw do
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
+  get 'signup' => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # 以下のコードを追加してみましょう
-  get 'hello-world' => "foo#hello"
+  
 
 end
