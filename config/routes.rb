dcc1eb806c270da2ca64f9793fbc5ebbbb173b81
cowatea <earlgrey.cafe.tea@gmail.com>
2021-12-18 00:00:54 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   get 'signup' => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
+  resources :users
 
 end
