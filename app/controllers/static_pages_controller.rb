2eab5a3b2207426cfe1eb7d2425fe56be3bbc00a
cowatea <earlgrey.cafe.tea@gmail.com>
2022-01-02 13:32:18 +0000

    tweet completed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,18 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+   def home
+     if logged_in?
+       @tweet = current_user.tweets.build
+       @tweets = Tweet.all.order(created_at: "DESC").paginate(page:params[:page], per_page: 10)  if logged_in?
+     end
+    end
 
-  def help
-  end
 
-  def about
-  end
+    def help
+    end
 
-  def contact
-  end
-end
+    def about
+    end
+
+    def contact
+    end
+ end
